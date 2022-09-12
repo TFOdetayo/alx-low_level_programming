36be5fdd16d8aea7a00ea45972f51565ca9e006e
TFOdetayo <tayeodetay@gmail.com>
2022-09-12 16:26:14 -0700

    Task1

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,11 +1,13 @@
 #include <stdio.h>
 /**
-* main - Printing
+* main - Prints "Programming is like building a multilingual
+* 		puzzle, followed by a new line.
 *
 * Return: Always 0 (Successful)
 */ 
 int main(void)
 {
 	puts("\"Programming is like building a multilingual puzzle");
+
 	return (0);
 }
--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -1,20 +1,23 @@
+/*
+ * File: 6-size.c
+ */
+
 #include <stdio.h>
+
 /**
- * Main - Prints the size of various types 
+ * Main - Prints the size of various types based on
+ * 	the computer it is compiled and run on
  *
- * Return: Always 0 (Success)
-*/
+ * Return: Always 0.
+ */
+
 int main(void)
 {	
-char a;
-int b;
-long int c;
-long long int d;
-float e;
-printf("size of char : %id byte(s)\n", sizeof(a));
-printf("Size of an int: %d byte(s)\n", sizeof(b));
-printf("Size of a long int: %d byte(s)\n", sizeof(c));
-printf("Size of a long long int: %d byte(s)\n", sizeof(d));
-printf("Size of a float: %d byte(s)\n", sizeof(e));
-return (0);
+	printf("size of char : %id byte(s)\n", sizeof(a));
+	printf("Size of an int: %d byte(s)\n", sizeof(b));
+	printf("Size of a long int: %d byte(s)\n", sizeof(c));
+	printf("Size of a long long int: %d byte(s)\n", sizeof(d));
+	printf("Size of a float: %d byte(s)\n", sizeof(e));
+
+	return (0);
 }
