9a722c11a62b6dfc8c46de867fb74c79baf1359c
TFOdetayo <tayeodetay@gmail.com>
2022-09-14 20:52:24 -0700

    AAA

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+/**
+ * main - a function that prints alphabets
+ * description: a function tht prints
+ *
+ * Return: 0
+ */
+
+void print_alphabet(void)
+{
+	char letter;
+
+	for (letter = 'a'; letter <= 'z'; letter++)
+		_putchar(letter);
+	_putchar('\n');
+}
