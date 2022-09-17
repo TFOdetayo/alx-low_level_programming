d7a38e9e1762d45b56561f5e0f477e94d5d1f104
TFOdetayo <tayeodetay@gmail.com>
2022-09-16 20:42:29 -0700

    Task6

---
+++ 0x04-more_functions_nested_loops/6-print_line.c
@@ -0,0 +1,25 @@
+#include "main.h"
+
+/**
+ * print_line - a function that draws a straight li
+ * @n: the argument.
+ * Return: 0.
+ */
+
+void print_line(int n)
+{
+	int a;
+
+	for (a = 0; a <= n; a++)
+	{
+		_putchar('_');
+	}
+	if (a <= 0)
+	{
+		_putchar(10);
+	}
+	else
+	{
+		_putchar(10);
+	}
+}
