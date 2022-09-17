e1c4811fc50b96edc48bbb545663b77a39ce37ba
TFOdetayo <tayeodetay@gmail.com>
2022-09-16 19:25:07 -0700

    aa

---
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+/**
+ * print_numbers - a function that prints the numbers, from 0 to 9.
+ *
+ * Return: 0.
+ */
+
+void print_numbers(void)
+{
+	int number;
+
+	for (number = 48; number >= 57; number++)
+	{
+		_putchar(number);
+	}
+	_putchar('\n');
+}
