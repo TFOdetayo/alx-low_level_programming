cddb5a27e3c26159e3b5262a1b05d394e81d3239
TFOdetayo <tayeodetay@gmail.com>
2022-09-20 21:57:23 -0700

    Task8

---
+++ 0x05-pointers_arrays_strings/5-rev_string.c
@@ -0,0 +1,14 @@
+#include "main.h"
+#include "string.h"
+
+/**
+ * rev_string - a function that reverses a string.
+ * @s: the parameter.
+ *
+ * Return: 0.
+ */
+
+void rev_string(char *s)
+{
+
+}
---
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -0,0 +1,13 @@
+#include "main.h"
+
+/**
+ * puts2 - prints every other character of a string.
+ * @str: the parameter.
+ *
+ * Return 0.
+ */
+
+void puts2(char *str)
+{
+
+}
