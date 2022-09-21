942f34ce2e5a8af8b010645bd8f2260639e026c2
TFOdetayo <tayeodetay@gmail.com>
2022-09-20 21:10:19 -0700

    Task4

--- 0x05-pointers_arrays_strings/4-print_rev.c
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -1,13 +1,19 @@
 #include "main.h"
+#include <string.h>
 
-/*
- * print_rev - prints a string, in reverse,
- *	followed by a new line.
- * @s: - the parameter.
+/**
+ * print_rev - prints a string, in reverseet number
+ * @s: the parameter.
  * Return: 0.
  */
 
 void print_rev(char *s)
 {
+	int c = strlen(s);
 
+	while (c--)
+	{
+		_putchar(*(s + c));
+	}
+	_putchar(10);
 }
