2fe1d35f44309af6b899fa44a0437d61f37828f3
TFOdetayo <tayeodetay@gmail.com>
2022-09-22 17:37:26 -0700

    Task7

--- 0x04-more_functions_nested_loops/9-fizz_buzz.c
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -14,16 +14,16 @@ int main(void)
 		if ((a % 3) == 0 && (a % 5) == 0)
 			printf("FizzBuzz");
 
-		else if ((num % 3) == 0)
+		else if ((a % 3) == 0)
 			printf("Fizz");
 
-		else if ((num % 5) == 0)
+		else if ((a % 5) == 0)
 			printf("Buzz");
 
 		else
-			printf("%d", num);
+			printf("%d", a);
 
-		if (num == 100)
+		if (a == 100)
 			continue;
 		printf(" ");
 	}
