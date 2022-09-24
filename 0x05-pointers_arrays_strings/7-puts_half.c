fd2ef5ebc94c37aae1b4313f2015c991b2eaa151
TFOdetayo <tayeodetay@gmail.com>
2022-09-23 21:05:27 -0700

    Task 7

--- 0x05-pointers_arrays_strings/7-puts_half.c
+++ 0x05-pointers_arrays_strings/7-puts_half.c
@@ -3,3 +3,30 @@
 /**
  * puts_half - prints half of a string, followed by a new line.
  * @str: the parameter.
+ *
+ * Return: 0.
+ */
+
+void puts_half(char *str)
+{
+	int index = 0, len = 0, n;
+
+	while (str[index++])
+		len++;
+	{
+		if ((len % 2) == 0)
+		{
+			n = len / 2;
+		}
+		else
+		{
+			n = (len + 1) / 2;
+		}
+		for (index = n; index < len; index++)
+		{
+			_putchar(str[index]);
+		}
+	}
+	_putchar('\n');
+}
+
