45aa65921df91bcc16e3f63338577e01e6215ca0
TFOdetayo <tayeodetay@gmail.com>
2022-09-26 12:53:19 -0700

    Tasks

--- 0x04-more_functions_nested_loops/7-print_diagonal.c
+++ 0x04-more_functions_nested_loops/7-print_diagonal.c
@@ -15,12 +15,9 @@ void print_diagonal(int n)
 	for (a = 0; a < n; a++)
 	{
 		for (b = 0; b < a; b++)
-		{
-			if (n > 0)
 			_putchar(' ');
-		}
 		_putchar('\\');
+
 		_putchar('\n');
 	}
-	_putchar('\n');
 }
