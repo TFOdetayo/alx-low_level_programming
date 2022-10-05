d07f83fe9613569c666dbca1e88954e1094ccea3
TFOetayo <tayeodetayo@gmail.com>
2022-10-04 21:28:51 -0700

    Task 10

---
+++ 0x06-pointers_arrays_strings/102-magic.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+
+/**
+ * main - The program prints a[2] = 98, followed by a new line.
+ *
+ * Return: success(0).
+ */
+
+int main(void)
+{
+	int n;
+	int a[5];
+	int *p;
+
+	a[2] = 1024;
+	p = &n;
+
+	printf("a[2] = %d\n", a[2]);
+
+	return (0);
+}
