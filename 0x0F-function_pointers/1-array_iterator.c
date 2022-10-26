23702bf5cf7bfeba98845b52cf3bd5caeb3d3286
TFOetayo <tayeodetayo@gmail.com>
2022-10-26 02:11:17 -0700

    It prints a name

---
+++ 0x0F-function_pointers/1-array_iterator.c
@@ -0,0 +1,14 @@
+#include "function_pointers.h"
+
+/**
+ * array_iterator - Executes a function given as a
+ *		parameter on each element of an array.
+ * @array: The array.
+ * @size: The size of array.
+ *
+ */
+
+void array_iterator(int *array, size_t size, void (*action)(int))
+{
+
+}
