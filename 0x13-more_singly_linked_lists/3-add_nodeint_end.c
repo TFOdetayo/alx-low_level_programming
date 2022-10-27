a0a6b739da19337fe8fd611eda81106f929bb320
TFOetayo <tayeodetayo@gmail.com>
2022-10-27 15:45:11 -0700

    Task 4 done

---
+++ 0x13-more_singly_linked_lists/3-add_nodeint_end.c
@@ -0,0 +1,15 @@
+#include "lists.h"
+
+
+
+/**
+ * add_nodeint_end - Adds a new node at the end.
+ * @head: A pointer
+ * @n: The integer
+ *
+ * Return: NULL.
+ */
+
+listint_t *add_nodeint_end(listint_t **head, const int n)
+{
+}
